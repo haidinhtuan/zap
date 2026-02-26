c2087ecd550b3286a2bbe9f91073822a0686758e
Tuan Hai Dinh <tuan-hai.dinh@ionos.com>
2026-02-26 15:15:01 +0100

    Clear unread notifications on room navigation
    Auto-clear unread count when navigating rooms with j/k/gg/G,
    removing the need to manually press r to mark as read.

--- src/app.rs
+++ src/app.rs
@@ -230,24 +230,30 @@ impl App {
                 if self.mode == Mode::Normal && !self.rooms.is_empty() {
                     self.selected_room = (self.selected_room + 1).min(self.rooms.len() - 1);
                     self.scroll_offset = 0;
+                    self.rooms[self.selected_room].unread_count = 0;
                 }
             }
             Action::RoomPrev => {
                 if self.mode == Mode::Normal {
                     self.selected_room = self.selected_room.saturating_sub(1);
                     self.scroll_offset = 0;
+                    if let Some(room) = self.rooms.get_mut(self.selected_room) {
+                        room.unread_count = 0;
+                    }
                 }
             }
             Action::RoomFirst => {
                 if self.mode == Mode::Normal && !self.rooms.is_empty() {
                     self.selected_room = 0;
                     self.scroll_offset = 0;
+                    self.rooms[self.selected_room].unread_count = 0;
                 }
             }
             Action::RoomLast => {
                 if self.mode == Mode::Normal && !self.rooms.is_empty() {
                     self.selected_room = self.rooms.len() - 1;
                     self.scroll_offset = 0;
+                    self.rooms[self.selected_room].unread_count = 0;
                 }
             }
             Action::OpenRoom => {
